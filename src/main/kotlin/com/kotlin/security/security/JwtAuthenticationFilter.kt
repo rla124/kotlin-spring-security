aa7e0c1dc4da02e7e1012a6177c8bdae5b647843
Subin <rla0010101@gmail.com>
2024-05-03 12:00:12 +0900

    feat[#2]: 로그인 JwtAuthenticationFilter 토큰에서 jwtService에서 구현한 메소드 이용하여 username 추출

--- src/main/kotlin/com/kotlin/security/security/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/kotlin/security/security/JwtAuthenticationFilter.kt
@@ -1,7 +1,37 @@
 package com.kotlin.security.security
 
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
-class JwtAuthenticationFilter {
+class JwtAuthenticationFilter(
+        private val jwtService: JwtService,
+        private val userDetailsService: UserDetailsService
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+
+        val log = LoggerFactory.getLogger(JwtAuthenticationFilter::class.java.name)
+
+        val authHeader = request.getHeader(HttpHeaders.AUTHORIZATION)
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        val lengthOfBearerWithSpace = 7
+        val jwtToken = authHeader.substring(lengthOfBearerWithSpace)
+
+        val username = jwtService.extractUsername(jwtToken)
+
+
+    }
+
 }
