de20e3b9e9ebe736d38def4c8445534da216662b
Subin <rla0010101@gmail.com>
2024-05-03 12:32:15 +0900

    feat[#2]: 로그인 JwtAuthenticationFilter 토큰에서 추출한 username으로 loadUserByUsername 이용해서 userDetails에 할당

--- src/main/kotlin/com/kotlin/security/security/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/kotlin/security/security/JwtAuthenticationFilter.kt
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
@@ -31,7 +32,10 @@ class JwtAuthenticationFilter(
 
         val username = jwtService.extractUsername(jwtToken)
 
+        if (SecurityContextHolder.getContext().authentication == null) {
 
+            val userDetails = userDetailsService.loadUserByUsername(username)
+        }
     }
 
 }
