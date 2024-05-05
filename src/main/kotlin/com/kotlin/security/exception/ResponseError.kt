10287824e862e687eabeecefa20f3d1f14c17418
Subin <rla0010101@gmail.com>
2024-05-05 10:16:19 +0900

    feat: ResponseError 중복 회원가입 conflict, 비인증 요청 unauthorized 추가

--- src/main/kotlin/com/kotlin/security/exception/ResponseError.kt
+++ src/main/kotlin/com/kotlin/security/exception/ResponseError.kt
@@ -5,5 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFormat
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 enum class ResponseError(val statusCode: Int, val message: String) {
 
-    ALREADY_REGISTERED(409, "Already registered.")
+    ALREADY_REGISTERED(409, "Already registered."),
+
+    BAD_CREDENTIALS(401, "Bad credentials")
 }
