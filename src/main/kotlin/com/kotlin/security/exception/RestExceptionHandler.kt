ac6a2e959802396678473a2b09d6f1bd4dfc2495
Subin <rla0010101@gmail.com>
2024-05-06 11:09:03 +0900

    feat: RestExceptionHandler httpStatus 409, 401 error 처리

--- src/main/kotlin/com/kotlin/security/exception/RestExceptionHandler.kt
+++ src/main/kotlin/com/kotlin/security/exception/RestExceptionHandler.kt
@@ -2,6 +2,10 @@ package com.kotlin.security.exception
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
@@ -9,5 +13,19 @@ class RestExceptionHandler {
 
     val log: Logger = LoggerFactory.getLogger(RestExceptionHandler::class.java.name)
 
-    // TODO: ExceptionHandler 처리
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    fun handleRegisteredException(e: RegisteredException): ResponseError {
+        log.error(e.message, e) // msg: String!, t: Throwable!
+        return ResponseError.ALREADY_REGISTERED
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    fun handleBadCredentialsException(e: BadCredentialsException): ResponseError {
+        log.error(e.message, e)
+        return ResponseError.BAD_CREDENTIALS
+    }
+
+    class RegisteredException: Exception()
 }
