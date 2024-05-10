d745593178b5d42f0bfb79eeec456b6a0be9ecb8
Subin <rla0010101@gmail.com>
2024-05-10 15:58:27 +0900

    chore[#3]: RedisConfig 설정

---
+++ src/main/kotlin/com/kotlin/security/redis/RedisConfig.kt
@@ -0,0 +1,25 @@
+package com.kotlin.security.redis
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+
+@Configuration
+@EnableRedisRepositories
+class RedisConfig {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory()
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<ByteArray, ByteArray> {
+        val redisTemplate = RedisTemplate<ByteArray, ByteArray>()
+        redisTemplate.connectionFactory = redisConnectionFactory()
+        return redisTemplate
+    }
+}
