8da1b521479c8f2b50d43a56a16c402206d2702f
Subin <rla0010101@gmail.com>
2024-05-14 12:42:14 +0900

    feat[#3]: 로그인 시 refresh token redis에 저장 완료

--- src/main/kotlin/com/kotlin/security/service/UserService.kt
+++ src/main/kotlin/com/kotlin/security/service/UserService.kt
@@ -5,8 +5,10 @@ import com.kotlin.security.exception.RestExceptionHandler
 import com.kotlin.security.model.authentication.AuthenticationRequest
 import com.kotlin.security.model.authentication.AuthenticationResponse
 import com.kotlin.security.model.authentication.RegisterRequest
+import com.kotlin.security.redis.RedisUtil
 import com.kotlin.security.repository.UserRepository
 import com.kotlin.security.security.JwtService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -17,8 +19,11 @@ class UserService(
         private val userRepository: UserRepository,
         private val passwordEncoder: PasswordEncoder,
         private val authenticationManager: AuthenticationManager,
-        private val jwtService: JwtService
+        private val jwtService: JwtService,
+        private val redisUtil: RedisUtil
 ) {
+    @Value("\${custom.jwt.token.refresh-expiration-time}")
+    lateinit var refreshExpirationTime: Number
     fun register(registerRequest: RegisterRequest): Unit {
 
         if (userRepository.findByUsernameOrEmail(registerRequest.username, registerRequest.email) != null) {
@@ -48,6 +53,10 @@ class UserService(
         val accessToken = jwtService.generateAccessToken(user!!)
         val refreshToken = jwtService.generateRefreshToken(user!!)
 
+        redisUtil.setData(user.username, refreshToken, refreshExpirationTime.toLong())
+
         return AuthenticationResponse(accessToken, refreshToken)
     }
+
+    // TODO: refresh token 이용한 토큰 재발급 함수 추가
 }
