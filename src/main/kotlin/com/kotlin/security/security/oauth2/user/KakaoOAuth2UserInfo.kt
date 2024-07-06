2d33b537d7979c6cf35e8ff7cff80ca9bd534b38
Subin <rla0010101@gmail.com>
2024-07-06 22:21:37 +0900

    feat[#5]: KakaoOAuth2UserInfo

---
+++ src/main/kotlin/com/kotlin/security/security/oauth2/user/KakaoOAuth2UserInfo.kt
@@ -0,0 +1,18 @@
+package com.kotlin.security.security.oauth2.user
+
+import com.kotlin.security.security.oauth2.OAuth2UserInfo
+
+class KakaoOAuth2UserInfo(attributes: MutableMap<String, Any>) : OAuth2UserInfo(attributes) {
+
+    override val id: String
+        get() = (attributes["id"] as Int).toString()
+
+    override val name: String
+        get() = (attributes["properties"] as Map<*, *>)["nickname"] as String
+
+    override val email: String
+        get() = (attributes["kakao_account"] as Map<*, *>)["email"] as String
+
+    override val imageUrl: String?
+        get() = (attributes["properties"] as Map<*, *>)["thumbnail_image"] as String?
+}
